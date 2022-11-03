98ae00c186dc0fa6595c42871cd7c7f4c9c3a454
SlavenkoArt <102622505+SlavenkoArt@users.noreply.github.com>
2022-11-03 15:26:25 +0300

    Задача 2. Кратность

---
+++ Задача 2. Кратность.py
@@ -0,0 +1,23 @@
+N = int(input('Количество чисел в списке: '))
+list_num = []
+count = 0
+index_sum = 0
+index_num = []
+
+for _ in range(N):
+    print('Введите', _ + 1, 'число:')
+    num = int(input())
+    list_num.append(num)
+
+divider = int(input('Введите делитель: '))
+
+for number in list_num:
+    if number % divider == 0:
+        index_num.append(number)
+
+for symbol in index_num:
+    count += 1
+    print('Индекс числа ', symbol, ': ', count)
+    index_sum += count
+
+print('Сумма индексов: ', index_sum)
