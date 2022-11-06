32814c049418a27d9e424a9f9027b7daabf748a3
SlavenkoArt <102622505+SlavenkoArt@users.noreply.github.com>
2022-11-06 16:11:59 +0300

    Задача 1. Генерация списка

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-def employees(emp):
-    id_employees = []
-    for _ in range(emp):
-        id = int(input('ID сотрудника: '))
-        id_employees.append(id)
-    return id_employees
-
-def counter(id_employees, search_id):
-    counter = 0
-    for symbol in id_employees:
-        if symbol == search_id:
-            print('Сотрудник на месте.')
-        elif symbol != search_id:
-            counter += 1
-    return counter
-
-emp = int(input('Количество сотрудников в офисе: '))
-
-emp_count = employees(emp)
-
-search_id = int(input('Какой ID ищем: '))
-
-counter = counter(emp_count, search_id)
-
-if counter == emp:
-    print('Сотрудник не работает!')
+number = 1
+for symbol in range(number + 1):
+    print(symbol)
